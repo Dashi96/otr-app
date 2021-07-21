c59ca67169309427139841bbddb01394d5d52a12
Dashi <ddashi96@mail.ru>
2021-07-22 00:32:17 +0700

    add indeterminate

--- src/app/node/node.component.ts
+++ src/app/node/node.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {accessRights} from '../app.component';
 
 @Component({
@@ -6,7 +6,43 @@ import {accessRights} from '../app.component';
   templateUrl: './node.component.html',
   styleUrls: ['./node.component.css']
 })
-export class NodeComponent {
+export class NodeComponent implements OnInit {
   @Input()
   node: accessRights | any;
+  allThings: any[] = [];
+  children: any[] = [];
+
+  ngOnInit(): void {
+    this.allThings = this.nodeArray('input');
+
+    addEventListener('change',  (e: any) => {
+      let check = e.target;
+
+      if(this.allThings.indexOf(check) === -1) {
+        return;
+      }
+
+      this.children = this.nodeArray('input', check.parentNode);
+
+      this.children.forEach(child => child.checked = check.checked);
+
+      while(check){
+        let parent = (check.closest(['node']).parentNode).querySelector('input');
+
+        const siblings: any[] = this.nodeArray('input', parent.closest('li').querySelector(['ul']));
+
+        const checkStatus = siblings.map(check => check.checked);
+        const every  = checkStatus.every(Boolean);
+        const some = checkStatus.some(Boolean);
+
+        parent.indeterminate = !every && every !== some;
+
+        check = check !== parent ? parent : 0;
+      }
+    });
+  }
+
+  nodeArray(selector: string, parent: Document = document) {
+    return [].slice.call(parent.querySelectorAll(selector));
+  }
 }
